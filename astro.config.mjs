e5a87e1fef4c03a1e6772d1ab76404327c959bcc
Carlos Cisneros <ccisneros@sisicom.com.mx>
2024-08-01 00:18:10 -0600

    Error de sintaxis

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/node";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind()],
   adapter: node(),
-   base: "/landing", // Añade esta línea
+  base: "/landing", // Añade esta línea
 });

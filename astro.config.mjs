be8fa7d57e1b2daad2ae22a951bd6386879b23e4
Carlos Cisneros <ccisneros@sisicom.com.mx>
2024-08-01 08:42:59 -0600

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,14 @@ export default defineConfig({
   output: "server",
   integrations: [tailwind()],
   adapter: node({ mode: "standalone" }), // Asegúrate de agregar esta línea
+  vite: {
+    ssr: {
+      noExternal: ["fs"], // Añade esto para evitar problemas con fs en el contexto SSR
+    },
+    build: {
+      rollupOptions: {
+        external: ["fs"], // Excluye fs del bundle
+      },
+    },
+  },
 });

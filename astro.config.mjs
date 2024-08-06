6bc74a5efd7a09a95643ff1a845419b694fb61a4
Carlos Cisneros <carloscisnerosad@gmail.com>
2024-08-06 03:23:14 -0600

    Configuración landing-notion

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,6 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
   integrations: [tailwind()],
-  adapter: node({ mode: "standalone" }), // Asegúrate de agregar esta línea
-  vite: {
-    ssr: {
-      noExternal: ["fs"], // Añade esto para evitar problemas con fs en el contexto SSR
-    },
-    build: {
-      rollupOptions: {
-        external: ["fs"], // Excluye fs del bundle
-      },
-    },
-  },
 });

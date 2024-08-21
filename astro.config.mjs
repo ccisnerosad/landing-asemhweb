e87c3740ce9dd6970ef77fb3007170228ec72981
Carlos Cisneros <carloscisnerosad@gmail.com>
2024-08-20 18:06:34 -0600

    Final landing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 
+//direcrorio /landing/
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  base: "/landing/",
   integrations: [tailwind()],
 });
